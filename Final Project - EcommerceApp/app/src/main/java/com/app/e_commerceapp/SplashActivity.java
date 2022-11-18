1fbdb123074956ebfea4bf4dc784cd9cf68eb5fb
OsamaAHH <osama616altaf@gmail.com>
2022-11-18 16:01:45 +0500

    I have created the project and created it's splash page.
    I have created a new project and Splash activity, where I have added the running man logo and it displays for 3 seconds and then moves on to the MainActivity.

---
+++ Final Project - EcommerceApp/app/src/main/java/com/app/e_commerceapp/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.app.e_commerceapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    new Timer().schedule(new TimerTask() {
+        @Override
+        public void run() {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+    }, 3000);
+
+    }
+}
