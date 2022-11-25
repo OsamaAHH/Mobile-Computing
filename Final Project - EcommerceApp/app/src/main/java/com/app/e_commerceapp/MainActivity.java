4eede09cda6edef8e9577cdb61fe393dcc77a464
OsamaAHH <osama616altaf@gmail.com>
2022-11-25 16:16:31 +0500

    Created Navigation Bar with multiple fragemnts
    I have created 5 fragments but will be using only 4 in the navbar. As the checkout fragment will be used within the Cart page.
    The 5 fragments created are:
    Home
    Shop
    Category
    Cart
    Checkout

--- Final Project - EcommerceApp/app/src/main/java/com/app/e_commerceapp/MainActivity.java
+++ Final Project - EcommerceApp/app/src/main/java/com/app/e_commerceapp/MainActivity.java
@@ -1,20 +1,60 @@
 package com.app.e_commerceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    ShopFragment shopFragment = new ShopFragment();
+    CategoryFragment categoryFragment = new CategoryFragment();
+    CartFragment cartFragment = new CartFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return;
+
+                    case R.id.shop:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, shopFragment).commit();
+                        return;
+
+                    case R.id.category:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, categoryFragment).commit();
+                        return;
+
+                    case R.id.cart:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, cartFragment).commit();
+                        return;
+
+                }
+
+            }
+        });
+
+    }
 
 }
